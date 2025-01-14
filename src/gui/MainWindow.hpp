7c677fbc76ae353ace30636d3ad08e2c990525ab
nathanchandanson <54677494+nathanchandanson@users.noreply.github.com>
2025-01-14 17:23:14 +0100

    Added a basic mockup of the GUI

--- src/gui/MainWindow.hpp
+++ src/gui/MainWindow.hpp
@@ -1,4 +1,23 @@
 #ifndef MAINWINDOW_HPP
 #define MAINWINDOW_HPP
+/*
+ *  This file contains all the headers of the object that represents our main window
+*/
+#include "WindowComponents.hpp"
+
+class MainWindow : public QWidget
+{
+    Q_OBJECT
+
+private:
+    BottomBar *m_bottomBar;
+
+    PageDisplay *m_pageViewer;
+
+public: 
+    MainWindow(QWidget *parent = 0);
+
+    void showImage(Image image) { m_pageViewer->showImage(image); }
+};
 
 #endif
