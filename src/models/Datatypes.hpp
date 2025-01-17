482bc59d84da37d2f49f9e97ad863af0dc599e0c
nathanchandanson <54677494+nathanchandanson@users.noreply.github.com>
2025-01-17 19:22:50 +0100

    Modified the Comic class to track the current and max page numbers. Added the getters and setters.

--- src/models/Datatypes.hpp
+++ src/models/Datatypes.hpp
@@ -8,7 +8,22 @@
 using Image = QPixmap;
 
 /* Defining the Comic type */
-using Comic = std::vector<QPixmap>;
+class Comic
+{
+private:
+    std::vector<Image> m_pages;
+    int m_pageNumber;               // La première page est à l'index 0 de m_pages
+    int m_maxPageNumber;
+
+public:
+    void setPageNumber(int page) { m_pageNumber = page; };
+    void setMaxPageNumber(int maxPage){ m_maxPageNumber = maxPage; };
+    void addPage(Image thePage, int thePageNumber) { m_pages.emplace(m_pages.begin() + thePageNumber, thePage); m_maxPageNumber++; };
+
+    int getPageNumber() { return m_pageNumber; };
+    int getMaxPageNumber() { return m_maxPageNumber; };
+    Image getPage() { return m_pages[m_pageNumber]; };
+};
 
 
 #endif
