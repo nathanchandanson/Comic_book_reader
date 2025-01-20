f075f5c0a3a277dde5de09aadda9bef721ad2585
nathanchandanson <54677494+nathanchandanson@users.noreply.github.com>
2025-01-20 16:02:00 +0100

    Most of the interface is fonctionnal. The slider bugs

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,32 @@
 #include <iostream>
+
 #include <QApplication>
 
 #include "Datatypes.hpp"
-#include "Archive.hpp"
 #include "MainWindow.hpp"
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    MainWindow widget;
 
-    Comic imageVector;
-    imageVector.push_back(QPixmap("../data/comics/testImage1.jpg"));
-    imageVector.push_back(QPixmap("../data/comics/testImage2.jpg"));
+    MainWindow theWindow;
+    Comic theComic;
+
+    // Initialisation du comic
+    theComic.setPageNumber(0);
+    theComic.setMaxPageNumber(0);
+    theComic.addPage(QPixmap("../data/comics/testImage1.jpg"), 0);
+    theComic.addPage(QPixmap("../data/comics/testImage2.jpg"), 1);    
+    
+    // Connexion des signaux et slots
+    // MainWindow vers Comic
+    QObject::connect(&theWindow, &MainWindow::nextPagePressed, &theComic, &Comic::incrementPageNumber);
+    QObject::connect(&theWindow, &MainWindow::previousPagePressed, &theComic, &Comic::decrementPageNumber);
+    QObject::connect(&theWindow, &MainWindow::pageSliderValueChanged, &theComic, &Comic::setPageNumber);
+    // Comic vers MainWindow
+    QObject::connect(&theComic, &Comic::pageChanged, &theWindow, &MainWindow::updatePageChanges);
     
 
-    widget.show();
+    theWindow.show();
     return app.exec();
 }
--- src/models/Datatypes.hpp
+++ src/models/Datatypes.hpp
@@ -2,27 +2,37 @@
 #define DATATYPES_HPP
 
 #include <vector>
+#include <QObject>
 #include <QPixmap>
 
 /* Defining the Image type */
 using Image = QPixmap;
 
 /* Defining the Comic type */
-class Comic
+class Comic : public QObject
 {
+    Q_OBJECT
+
 private:
     std::vector<Image> m_pages;
-    int m_pageNumber;               // La première page est à l'index 0 de m_pages
-    int m_maxPageNumber;
+    unsigned int m_pageNumber;               // La première page est à l'index 0 de m_pages
+    unsigned int m_maxPageNumber;
 
 public:
-    void setPageNumber(int page) { m_pageNumber = page; };
     void setMaxPageNumber(int maxPage){ m_maxPageNumber = maxPage; };
     void addPage(Image thePage, int thePageNumber) { m_pages.emplace(m_pages.begin() + thePageNumber, thePage); m_maxPageNumber++; };
 
-    int getPageNumber() { return m_pageNumber; };
-    int getMaxPageNumber() { return m_maxPageNumber; };
+    unsigned int getPageNumber() { return m_pageNumber; };
+    unsigned int getMaxPageNumber() { return m_maxPageNumber; };
     Image getPage() { return m_pages[m_pageNumber]; };
+
+signals:
+    void pageChanged(Image newPage, int newPageNumber, int maxPage);
+
+public slots:
+    void setPageNumber(int page);
+    void incrementPageNumber();
+    void decrementPageNumber();
 };
 
 
