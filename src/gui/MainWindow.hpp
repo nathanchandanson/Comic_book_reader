c1d9fbccb9c8324a61c13f5dc78b7bf86165f8dd
nathanchandanson <54677494+nathanchandanson@users.noreply.github.com>
2025-01-20 16:02:00 +0100

    Most of the interface is fonctionnal. The slider bugs

--- src/gui/MainWindow.hpp
+++ src/gui/MainWindow.hpp
@@ -1,4 +1,31 @@
 #ifndef MAINWINDOW_HPP
 #define MAINWINDOW_HPP
+/*
+ *  This file contains all the headers of the object that represents our main window
+*/
+#include "WindowComponents.hpp"
+
+class MainWindow : public QWidget
+{
+    Q_OBJECT
+
+private:
+    BottomBar *m_bottomBar;
+
+    PageDisplay *m_pageViewer;
+
+public: 
+    MainWindow(QWidget *parent = 0);
+
+    void showImage(Image image) { m_pageViewer->showImage(image); }
+
+signals: // Lier avec les signaux des components.
+    void nextPagePressed();
+    void previousPagePressed();
+    void pageSliderValueChanged(int page);
+
+public slots:
+    void updatePageChanges(Image newPage, int newPageNumber, int maxPage);
+};
 
 #endif
